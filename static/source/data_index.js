19d9ba2e6b96dbb73d6d31682b6439e3bbf44a17
Ragnar-F <1332321+Ragnar-F@users.noreply.github.com>
2020-08-09 09:59:08 +0200

    v2.0-a120-07960753

--- static/source/data_index.js
+++ static/source/data_index.js
@@ -855,6 +855,7 @@ indexData = [
   ["SendPlay","commands/Send.htm#SendPlayDetail",2],
   ["SendText","commands/Send.htm",2],
   ["Set-Direktive (Ahk2Exe)","misc/Ahk2ExeDirectives.htm#Set",99],
+  ["Set-Methode (Map)","objects/Map.htm#Set"],
   ["SetCapacity-Methode (Objekt)","objects/Object.htm#SetCapacity"],
   ["SetCapsLockState","commands/SetNumScrollCapsLockState.htm",2],
   ["SetControlDelay","commands/SetControlDelay.htm",2],
