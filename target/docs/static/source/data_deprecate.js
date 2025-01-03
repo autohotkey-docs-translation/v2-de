d6a4295f9776932fc033ba4c33d9ca72e2f6b1c8
Ragnar-F <1332321+Ragnar-F@users.noreply.github.com>
2025-01-03 15:29:41 +0100

    Project translation

--- target/docs/static/source/data_deprecate.js
+++ target/docs/static/source/data_deprecate.js
@@ -1,3 +1,6 @@
 deprecateData = {
-  
+  "Hotstrings.htm#SP":"die Standard-Hotstring-Option SI",
+  "lib/Send.htm#SendPlay":"SendInput",
+  "lib/Send.htm#SendPlayDetail":"SendInput",
+  "lib/SendMode.htm#Play":"den Standardmodus Input"
 };
