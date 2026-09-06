d552b7a1eba43cf33334f4b79bf547b7554e2694
Ragnar-F <1332321+Ragnar-F@users.noreply.github.com>
2026-09-06 08:28:25 +0200

    Project translation

--- target/docs/static/source/data_deprecate.js
+++ target/docs/static/source/data_deprecate.js
@@ -1,5 +1,5 @@
 deprecateData = {
-  "Hotstrings.htm#SP":"die Standard-Hotstring-Option SI",
-  "lib/Send.htm#SendPlay":"SendInput oder SendEvent",
-  "lib/SendMode.htm#Play":"die anderen Sendemodi"
+  "Hotstrings.htm#SP": "die Standard-Hotstring-Option SI",
+  "lib/Send.htm#SendPlay": "SendInput oder SendEvent",
+  "lib/SendMode.htm#Play": "die anderen Sendemodi"
 };
---
+++ target/docs/static/source/data_docs.js
@@ -0,0 +1,21 @@
+docsData = {
+  LANGUAGE: "de", // ISO 639-1 language name standard
+  VERSION: "v2",
+  PRE_RELEASE: false,
+  TOOL_LANGUAGE_ITEMS: [
+    ["en", "https://www.autohotkey.com/docs/v2/", "English"],
+    ["de", "https://ahkde.github.io/docs/v2/", "Deutsch (German)"],
+    ["ja", "https://ahkscript.github.io/ja/docs/v2/", "日本語 (Japanese)"],
+    ["zh", "https://wyagd001.github.io/v2/docs/", "中文 (Chinese)"]
+  ],
+  TOOL_VERSION_ITEMS: [
+    ["v1", "https://ahkde.github.io/docs/v1/", "AutoHotkey v1.1"],
+    ["v2", "https://ahkde.github.io/docs/v2/", "AutoHotkey v2.0"]
+  ],
+  // TOOL_VERSION_ITEM_PRE: null,
+  TOOL_VERSION_ITEM_PRE: ["https://www.autohotkey.com/docs/alpha/", "AutoHotkey v2.1 Vorabversion"],
+  TOOL_EDIT_LINK: "https://github.com/autohotkey-docs-translation/v2-de/edit/master/target/docs/",
+  TAB_ALT_SHORTCUT_CONTENT: "I",
+  TAB_ALT_SHORTCUT_INDEX: "N",
+  TAB_ALT_SHORTCUT_SEARCH: "S"
+};
